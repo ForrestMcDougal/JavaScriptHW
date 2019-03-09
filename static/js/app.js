160a4a88e746063edacb68a028e0f1267eab878a
Forrest McDougal <forrestmcdougal@gmail.com>
2019-03-09 09:57:12 -0500

    cleanup, comment, and fix filtering

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,9 @@
 // from data.js
 let tableData = data;
 
+/**
+ * Function to proper (title) case a string
+ */
 String.prototype.toProperCase = function() {
 	return this.replace(/\w\S*/g, function(txt) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -16,30 +19,42 @@ const shapeDropdown = d3.select('#shape-dropdown');
 let states = [];
 let shapes = [];
 
+/**
+ * Go through all of the data and append state and shape
+ * to array if not present. This is used in the dropdowns.
+ * ALso clean up HTML codes that come with data.
+ */
 tableData.forEach((row) => {
+	row.state = row.state.toUpperCase();
+	row.city = row.city.toProperCase();
+
+	// add state and shape to respective array
 	if (!states.includes(row.state)) {
 		states.push(row.state);
 	}
 	if (!shapes.includes(row.shape)) {
 		shapes.push(row.shape);
 	}
+
+	// clean up html codes
 	row.comments = row.comments.replace(/&#44/g, ',');
 	row.comments = row.comments.replace(/&#39/g, "'");
 	row.comments = row.comments.replace(/&#33/g, '!');
 
+	// delete the country attribute due to redundancy
 	delete row.country;
-
-	row.state = row.state.toUpperCase();
-	row.city = row.city.toProperCase();
 });
 
 states.sort();
 shapes.sort();
 
+/**
+ * Add options to the dropdowns for each state and shape
+ */
 states.forEach((state) => {
 	let option = stateDropdown.append('option');
 	option.attr('value', state);
-	option.text(state.toUpperCase());
+	option.text(state);
 });
 
 shapes.forEach((shape) => {
@@ -48,11 +63,19 @@ shapes.forEach((shape) => {
 	option.text(shape);
 });
 
+/**
+ * showTable is used to paint the table onto the DOM
+ * Take in a dataset and append rows for each data point
+ * @param {object} table 
+ */
 function showTable(table) {
+	// clear all of the table html
 	tbody.html('');
+
+	// add in row for each entry
 	table.forEach((datum) => {
 		let row = tbody.append('tr');
-		Object.entries(datum).forEach(([ key, value ]) => {
+		Object.entries(datum).forEach(([ k, value ]) => {
 			var cell = row.append('td');
 			cell.text(value);
 		});
@@ -61,6 +84,10 @@ function showTable(table) {
 
 showTable(tableData);
 
+/**
+ * go through all of the filters progressively to 
+ * get only targeted data.
+ */
 function filterAll() {
 	// filter by date
 	d3.event.preventDefault();
@@ -87,10 +114,21 @@ function filterAll() {
 	showTable(table);
 }
 
+/**
+ * remove all filters and show original table
+ */
 function unfilter() {
 	d3.event.preventDefault();
+
+	// remove all filters
+	document.getElementById('datetime').value = '';
+	document.getElementById('state-dropdown').selectedIndex = 0;
+	document.getElementById('shape-dropdown').selectedIndex = 0;
+
+	// show original table
 	showTable(tableData);
 }
 
+// event handlers
 button.on('click', filterAll);
 clearFilter.on('click', unfilter);
